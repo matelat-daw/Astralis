59c56cca992ea237bef82bca71be738ec00fb076
matelat-daw <cesarmatelat@gmail.com>
2025-05-05 13:11:12 +0100

    Update Again.

---
+++ .angular/cache/19.2.10/Astralis/vite/deps_ssr/@angular_platform-server.js
@@ -0,0 +1,39 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BEFORE_APP_SERIALIZED,
+  ENABLE_DOM_EMULATION,
+  INITIAL_CONFIG,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS,
+  PlatformState,
+  SERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderInternal,
+  renderModule
+} from "./chunk-EXKDTKYH.js";
+import "./chunk-BS5VWWXT.js";
+import "./chunk-MXCYHD5U.js";
+import "./chunk-JAL6QZ4N.js";
+import "./chunk-NJ6MY3XD.js";
+import "./chunk-AT4BY72T.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  BEFORE_APP_SERIALIZED,
+  INITIAL_CONFIG,
+  PlatformState,
+  ServerModule,
+  VERSION,
+  platformServer,
+  provideServerRendering,
+  renderApplication,
+  renderModule,
+  ENABLE_DOM_EMULATION as ɵENABLE_DOM_EMULATION,
+  INTERNAL_SERVER_PLATFORM_PROVIDERS as ɵINTERNAL_SERVER_PLATFORM_PROVIDERS,
+  SERVER_CONTEXT as ɵSERVER_CONTEXT,
+  SERVER_RENDER_PROVIDERS as ɵSERVER_RENDER_PROVIDERS,
+  renderInternal as ɵrenderInternal
+};
